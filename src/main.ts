0c63430c29c3a24a70565fb689a17b9a61c1289c
dhanuskumar <dhanuskumar18@gmail.com>
2025-11-06 12:33:35 +0530

    cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5002',
+      'https://eastlandadmin.webnoxdigital.com',
       'http://localhost:8000',
       'http://localhost:3001',
       'http://localhost:5173', // Vite default port
