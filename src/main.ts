fcb4e83a35ad85b0af889b3f1fe454925c18ba4b
dhanuskumar <dhanuskumar18@gmail.com>
2025-11-06 10:41:36 +0530

    port

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   // CORS Configuration
   app.enableCors({
     origin: [
-      'http://localhost:5000',
+      'http://localhost:5002',
       'http://localhost:8000',
       'http://localhost:3001',
       'http://localhost:5173', // Vite default port
